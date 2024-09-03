4c07f612c569df4f1e29108089a54110633bb66d
Gabriel Henrique <101333974+gblzera@users.noreply.github.com>
2024-09-03 09:16:54 -0300

    Update Aula3.java

--- port/POO.java/Aula3.java
+++ port/POO.java/Aula3.java
@@ -4,13 +4,14 @@ public class Aula3 {
     public static void main(String [] args){
         int num1 = 0;
         int num2 = 1;
-        int proxNum = 0;
+        int proxNum;
         int n = 15;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Os primeiros 15 termos da sequência de Fibonacci são :");
-        System.out.println(num1 + num2);
-        for (int i = 1; i < n; i++){
+        System.out.println(num1);
+        System.out.println(num2);
+        for (int i = 2; i < n; i++){
             proxNum = num1 + num2;
             num1 = num2;
             num2 = proxNum;
