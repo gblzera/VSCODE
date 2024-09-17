7cfe0dcb81317df3db7d613235bcc68da6fd5520
Gabriel <gabriel.kpaz@gmail.com>
2024-09-16 21:45:22 -0300

    exercicio8.provaP1 v3

--- Java/provaP1/Main.java
+++ Java/provaP1/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Veiculo veiculo = new Veiculo("Toyota", "Corolla", "P4L2RT0", 2022, "14230", "João Paulo", "123.123.123-12");
-        OrdemServico ordemServico = new OrdemServico(1, "Betinho", "Flavin do pneu", true, false, "Barulho na roda dianteira");
+        OrdemServico ordemServico = new OrdemServico(1, "Betinho", "Bruno Maranhão", true, false, "Barulho na roda dianteira");
 
         LocalDate dataEntrega = LocalDate.now();
         double valorConcerto = 1500.75;
